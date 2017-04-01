e88291c5a834e29e3c94a741e907f628b6fd5ee2
rkhasano <rkhasano@online.jccc.edu>
2017-04-01 16:50:46 -0500

    Update hp3_program.cpp

--- hp3_program.cpp
+++ hp3_program.cpp
@@ -9,12 +9,12 @@ using namespace std;
 
 
 /* Setup() - 	Create the list of locations
- *              Allocate needed memory
- * */
+*              Allocate needed memory
+* */
 void Program::Setup()
 {
-    // This is called automatically when the program begins
-	
+	// This is called automatically when the program begins
+
 	// Creating dynamic array with size 6 for room locations.
 	Location* locationList;
 	locationList = new Location[6];
@@ -23,7 +23,7 @@ void Program::Setup()
 	locationList[0].name = "Treasure Room";
 	locationList[0].description = "There is a lot of gold here!";
 	locationList[1].name = "Court Yard";
-	locationList[1].description = "Many people we hanged here in the past. There are bones here.";
+	locationList[1].description = "Many people were hanged here in the past. There are bones here.";
 	locationList[2].name = "The hall";
 	locationList[2].description = "Very big room. There are arm-chair and sofa to rest.";
 	locationList[3].name = "Kitchen.";
@@ -34,10 +34,10 @@ void Program::Setup()
 	locationList[5].description = "There are big windows in this room to monitor what happens outside.";
 
 	// Location 1. Starting point. Possible to move only to  right(east).
-	locationList[1].ptrToEast = &locationList[2]; 
-	
+	locationList[1].ptrToEast = &locationList[2];
+
 	// From Location 2 back to west to 1, or south to location 4.
-	locationList[2].ptrToWest = &locationList[1]; 
+	locationList[2].ptrToWest = &locationList[1];
 	locationList[2].ptrToSouth = &locationList[4];
 
 	// From 4 back to North tp location 2, or east to 5.
@@ -58,36 +58,94 @@ void Program::Setup()
 
 	ptrCurrentLocation = &locationList[1];
 	ptrEndLocation = &locationList[0];
+
 }
 
 
 
 /* Cleanup() - 	Clean up memory allocated
- * */
+* */
 void Program::Cleanup()
 {
-    // This is called automatically when the program ends
-	if (locationList!= nullptr) 
+	// This is called automatically when the program ends
+	if (locationList != nullptr)
 	{
-		delete []locationList;
+		delete[]locationList;
 	}
 }
 
 
 
 /* Main() - Main program loop
- * */
+* */
 void Program::Main()
 {
+	bool done = false;
+	while (!done)
+	{
+		// Display Current Location
+		DisplayCurrentLocation();
+		
+		// Check if a user have reached the end
+		if (ptrCurrentLocation==ptrEndLocation)
+		{
+			cout << "You have reached your destination! Congratulations!";
+			
+			// Break the loop
+			break;
+		}
+
+		// Get the user choice
+		string userChoice;
+		userChoice = GetUserInput();
+
+		// If user's choice is North check if he can move that way.
+		if (userChoice == "n" && ptrCurrentLocation->ptrToNorth != nullptr) 
+			{
+				ptrCurrentLocation = ptrCurrentLocation->ptrToNorth;
+			}
+		
+		// If user's choice is East check if he can move that way.
+		else if (userChoice == "e" && ptrCurrentLocation->ptrToEast != nullptr)
+			{ 
+			ptrCurrentLocation = ptrCurrentLocation->ptrToEast;
+			}
+		
+		// If user's choice is South check if he can move that way.
+		else if (userChoice == "s" && ptrCurrentLocation->ptrToSouth != nullptr)
+			{
+				ptrCurrentLocation = ptrCurrentLocation->ptrToSouth;
+			}
+		
+		// If user's choice is West check if he can move that way.
+		else if (userChoice == "w" && ptrCurrentLocation->ptrToWest != nullptr)
+			{
+				ptrCurrentLocation = ptrCurrentLocation->ptrToWest;
+			}
+
+		// Check if user wants to leave the game
+		else if (userChoice == "q") 
+			{
+				done = true;
+			}
+		
+		// If none of the above is true inform user that he is doing something wrong
+		else 
+		   {
+			cout << "Invalid Choice!";
+		    } 
+	}
+	cout << endl << "GOOD BYE!" << endl;
+	
 }
 
 
 
 /* DisplayCurrentLocation() - Use the current location
- *              pointer to display the location's name
- *              and description, as well as which
- *              directions the player can move next.
- * */
+*              pointer to display the location's name
+*              and description, as well as which
+*              directions the player can move next.
+* */
 void Program::DisplayCurrentLocation()
 {
 	cout << ptrCurrentLocation->name << endl; //Display name of the room.
@@ -97,10 +155,10 @@ void Program::DisplayCurrentLocation()
 	move = "You can move *";
 
 	// Check which directions are null
-	if (ptrCurrentLocation->ptrToNorth != nullptr)  
-	 {
+	if (ptrCurrentLocation->ptrToNorth != nullptr)
+	{
 		move = move + "North ";
-	 }
+	}
 	if (ptrCurrentLocation->ptrToEast != nullptr)
 	{
 		move = move + "East ";
@@ -123,17 +181,17 @@ void Program::DisplayCurrentLocation()
 
 
 /* GetUserInput() - Display the list of options to the
- *                  user, then get their input and return
- *                  their choice.
- * */
+*                  user, then get their input and return
+*                  their choice.
+* */
 string Program::GetUserInput()
 {
 	string option;
 
 	cout << "Options to move: n to North, e to East, s to South, w to West. q to quit ";
 	cin >> option;
-	
-	return option; 
+
+	return option;
 }
 
 
@@ -144,12 +202,12 @@ string Program::GetUserInput()
 
 Program::Program()
 {
-    locationList = nullptr;
-    ptrCurrentLocation = nullptr;
-    Setup();
+	locationList = nullptr;
+	ptrCurrentLocation = nullptr;
+	Setup();
 }
 
 Program::~Program()
 {
-    Cleanup();
+	Cleanup();
 }
